fc8b427cc2adade8f2ec0aef845cb6b6091c9845
Amoriss <amoriss@protonmail.com>
2022-04-23 00:39:54 -0500

    initial commit -- models completed

---
+++ BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        //with Mock implementation instead of database
+
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription = "Lorem ", LongDescription = "Lorem"},
+                new Pie {PieId = 2, Name="Cheese cake", Price=18.95M, ShortDescription = "Lorem ", LongDescription = "Lorem"},
+                new Pie {PieId = 3, Name="Rhubarb Pie", Price=15.95M, ShortDescription = "Lorem ", LongDescription = "Lorem"},
+                new Pie {PieId = 4, Name="Pumpkin Pie", Price=12.95M, ShortDescription = "Lorem ", LongDescription = "Lorem"}
+
+            };            
+            
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+       
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
